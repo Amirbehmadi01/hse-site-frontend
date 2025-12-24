7bc7914e9f8744cf48505ae23e0a76efda0a4c67
Amir Behmadi <Behmadiamir7@gmail.com>
2025-12-24 21:08:56 +0330

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -174,7 +174,68 @@
 
 // 12/9
 
-// src/context/AuthContext.jsx
+
+
+//ورژن مشکل دار لاگین
+// // src/context/AuthContext.jsx
+// import { createContext, useContext, useState, useEffect } from "react";
+// import API from "../services/api.js";
+
+// const AuthContext = createContext();
+
+// export const useAuth = () => useContext(AuthContext);
+
+// export const AuthProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   // همیشه استارت در حالت logout باشد (هیچ چیز ذخیره نشود)
+//   useEffect(() => {
+//     setUser(null);
+//     localStorage.removeItem("user");
+//     setLoading(false);
+//   }, []);
+
+//   const login = async (username, password) => {
+//     try {
+//       // const res = await API.post("/auth/login", { username, password });
+
+//       const res = await API.post("/api/auth/login", { username, password });
+
+// /////////////
+
+
+//       const userData = res.data.user;
+//       setUser(userData);
+//       return { success: true, user: userData };
+//     } catch (err) {
+//       return { success: false, message: err.response?.data?.message || "خطا در ورود" };
+//     }
+//   };
+
+//   const logout = () => {
+//     setUser(null);
+//     localStorage.removeItem("user");
+//   };
+
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         user,
+//         login,
+//         logout,
+//         loading,
+//         isAdmin: user?.role === "Admin",
+//         isUser: user?.role === "User",
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+
+
 import { createContext, useContext, useState, useEffect } from "react";
 import API from "../services/api.js";
 
@@ -186,7 +247,6 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // همیشه استارت در حالت logout باشد (هیچ چیز ذخیره نشود)
   useEffect(() => {
     setUser(null);
     localStorage.removeItem("user");
@@ -195,13 +255,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (username, password) => {
     try {
-      // const res = await API.post("/auth/login", { username, password });
-
-      const res = await API.post("/api/auth/login", { username, password });
-
-/////////////
-
-
+      const res = await API.post("/auth/login", { username, password }); // ⚡ مسیر درست شد
       const userData = res.data.user;
       setUser(userData);
       return { success: true, user: userData };
