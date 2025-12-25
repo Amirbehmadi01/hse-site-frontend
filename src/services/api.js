c0fcac3acec114b9a5bbe6e25edf18d51d2cf579
Amir Behmadi <Behmadiamir7@gmail.com>
2025-12-25 18:16:21 +0330

    Update api.js 8888

--- src/services/api.js
+++ src/services/api.js
@@ -17,12 +17,12 @@
 
 // export default API;
 
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ❌ بدون + "/api"
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
 export default API;
+
